9be8203cbdcdb1cc56b3bc5e45f72ae1c09c116b
YasamNik <96878769+YasamNik@users.noreply.github.com>
2023-01-06 16:06:18 -0500

    initial commit

---
+++ streamlit_test.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import openai
+openai.api_key = "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"
+
+def ask_question(prompt):
+  completions = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+   stop=None,
+    temperature=0.5,
+  )
+
+  message = completions.choices[0].text
+  return message
+
+# Title and header
+st.title("AI Email Assistant")
+st.header("Enter row email body text")
+
+# Create a text input field
+text_input = st.text_area("Enter email text here :")
+popp = "askdjhbkf"
+# Display the text that the user entered
+#st.write(f"You entered: {text_input}")
+
+# Add a button to run some logic
+if st.button(f"Process {popp}"):
+    # Do something with the text
+    prompt = "Please generate a long polite style email from following text: "
+    prompt += text_input
+    result = ask_question(prompt)
+    st.write(f"Result: {result}")
